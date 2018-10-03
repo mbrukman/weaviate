9af5ef8b96613c9cc446ccfa3939547fb4865aba
Maarten Hoogendoorn <maarten@moretea.nl>
2018-10-03 16:57:30 +0200

    gh-486: Temporary schema query to get unit tests up to speed

---
+++ restapi/handlers_schema.go
@@ -0,0 +1,17 @@
+package restapi
+
+import (
+	"github.com/creativesoftwarefdn/weaviate/restapi/operations"
+	"github.com/creativesoftwarefdn/weaviate/restapi/operations/schema"
+	middleware "github.com/go-openapi/runtime/middleware"
+)
+
+func setupSchemaHandlers(api *operations.WeaviateAPI) {
+	api.SchemaWeaviateSchemaDumpHandler = schema.WeaviateSchemaDumpHandlerFunc(func(params schema.WeaviateSchemaDumpParams, principal interface{}) middleware.Responder {
+		payload := &schema.WeaviateSchemaDumpOKBody{
+			Actions: databaseSchema.ActionSchema.Schema,
+			Things:  databaseSchema.ThingSchema.Schema,
+		}
+		return schema.NewWeaviateSchemaDumpOK().WithPayload(payload)
+	})
+}
